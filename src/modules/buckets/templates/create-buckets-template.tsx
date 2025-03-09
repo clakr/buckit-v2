3b3f35bc3b9ff649c1070922f56dfa5e0cd342f1
clakr <clarktolosa@gmail.com>
2025-03-09 18:50:08 +0800

    chore: use tanstack form and vod for form creation and validation

--- src/modules/buckets/templates/create-buckets-template.tsx
+++ src/modules/buckets/templates/create-buckets-template.tsx
@@ -4,64 +4,156 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { useCreateBucketsMutation } from "@/modules/buckets/hooks";
+import { useForm } from "@tanstack/react-form";
 import { useNavigate } from "@tanstack/react-router";
-import React from "react";
+import { z } from "zod";
 
 export default function CreateBucketTemplate() {
   const navigate = useNavigate();
 
-  const name = React.useId();
-  const description = React.useId();
-  const currentAmount = React.useId();
-
   const { mutateAsync, isPending } = useCreateBucketsMutation();
 
-  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
+  const schema = z.object({
+    name: z
+      .string()
+      .min(1, "Name is required")
+      .max(255, "Name must be less than 255 characters"),
+    description: z
+      .string()
+      .max(1000, "Description must be less than 1000 characters"),
+    current_amount: z.coerce
+      .number()
+      .min(-1_000_000_000, "Amount must be at least -1,000,000,000") // Lower limit
+      .max(1_000_000_000, "Amount must be less than 1,000,000,000"), // Upper limit
+  });
 
-    const formData = new FormData(event.currentTarget);
+  const form = useForm({
+    defaultValues: {
+      name: "",
+      description: "",
+      current_amount: "",
+    },
+    validators: {
+      onSubmit: ({ value }) => {
+        const { success, error } = schema.safeParse(value);
 
-    await mutateAsync({
-      name: formData.get("name")?.toString() ?? "",
-      description: formData.get("description")?.toString() ?? "",
-      current_amount: +(formData.get("currentAmount") ?? 0),
-    });
+        if (!success) {
+          return {
+            fields: error.flatten().fieldErrors,
+          };
+        }
+      },
+    },
+    onSubmit: async ({ value }) => {
+      const payload = schema.parse(value);
 
-    navigate({
-      to: "/buckets",
-    });
-  }
+      await mutateAsync(payload);
+
+      navigate({
+        to: "/buckets",
+      });
+    },
+  });
 
   return (
     <Main className="grid gap-y-4">
       <h1 className="text-3xl font-bold">Create Bucket</h1>
-      <form className="grid gap-y-3" onSubmit={handleSubmit}>
-        <fieldset className="grid gap-y-1.5">
-          <Label htmlFor={name}>Name</Label>
-          <Input id={name} type="text" name="name" placeholder="Enter name" />
-        </fieldset>
-        <fieldset className="grid gap-y-1.5">
-          <Label className="items-end gap-x-1" htmlFor={description}>
-            Description <small>(optional)</small>
-          </Label>
-          <Textarea
-            id={description}
-            name="description"
-            placeholder="Enter description"
-            rows={5}
-          />
-        </fieldset>
-        <fieldset className="grid gap-y-1.5">
-          <Label htmlFor={currentAmount}>Current Amount</Label>
-          <Input
-            id={currentAmount}
-            type="number"
-            name="currentAmount"
-            step="0.01"
-            placeholder="Enter current amount"
-            defaultValue={0}
-          />
-        </fieldset>
+      <form
+        className="grid gap-y-3"
+        onSubmit={(event) => {
+          event.preventDefault();
+          event.stopPropagation();
+          form.handleSubmit();
+        }}
+      >
+        <form.Field
+          name="name"
+          children={(field) => (
+            <fieldset className="group grid grid-cols-2 gap-y-1.5">
+              <Label
+                htmlFor={field.name}
+                className="group-has-[em]:text-destructive"
+              >
+                Name
+              </Label>
+              {field.state.meta.errors.length > 0 ? (
+                <em
+                  role="alert"
+                  className="text-destructive text-end text-sm/none"
+                >
+                  {field.state.meta.errors.join(", ")}
+                </em>
+              ) : null}
+              <Input
+                id={field.name}
+                type="text"
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                onChange={(e) => field.handleChange(e.target.value)}
+                className="group-has-[em]:border-destructive col-span-full"
+              />
+            </fieldset>
+          )}
+        />
+        <form.Field
+          name="description"
+          children={(field) => (
+            <fieldset className="group grid grid-cols-2 gap-y-1.5">
+              <Label
+                htmlFor={field.name}
+                className="group-has-[em]:text-destructive items-end gap-x-1"
+              >
+                Description
+                <small>(optional)</small>
+              </Label>
+              {field.state.meta.errors.length > 0 ? (
+                <em
+                  role="alert"
+                  className="text-destructive text-end text-sm/none"
+                >
+                  {field.state.meta.errors.join(", ")}
+                </em>
+              ) : null}
+              <Textarea
+                id={field.name}
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                onChange={(e) => field.handleChange(e.target.value)}
+                rows={5}
+                className="group-has-[em]:border-destructive col-span-full"
+              />
+            </fieldset>
+          )}
+        />
+        <form.Field
+          name="current_amount"
+          children={(field) => (
+            <fieldset className="group grid grid-cols-2 gap-y-1.5">
+              <Label
+                htmlFor={field.name}
+                className="group-has-[em]:text-destructive"
+              >
+                Current Amount
+              </Label>
+              {field.state.meta.errors.length > 0 ? (
+                <em
+                  role="alert"
+                  className="text-destructive text-end text-sm/none"
+                >
+                  {field.state.meta.errors.join(", ")}
+                </em>
+              ) : null}
+              <Input
+                id={field.name}
+                type="number"
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                onChange={(e) => field.handleChange(e.target.value)}
+                className="group-has-[em]:border-destructive col-span-full"
+              />
+            </fieldset>
+          )}
+        />
         <LoadingButton
           type="submit"
           className="justify-self-end"
