c712f6f81410e72b4bce680ea61e078fad43b4c3
clakr <clarktolosa@gmail.com>
2025-03-09 10:10:19 +0800

    feat(login): implemented ui

--- src/modules/authentication/templates/login-template.tsx
+++ src/modules/authentication/templates/login-template.tsx
@@ -1,3 +1,67 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
+import { Icon } from "@iconify/react";
+import { Link } from "@tanstack/react-router";
+import React from "react";
+
 export default function LoginTemplate() {
-  return <div>login!</div>;
+  const email = React.useId();
+  const password = React.useId();
+
+  return (
+    <main className="grid min-h-svh place-content-center gap-y-6 *:max-w-sm">
+      <section className="grid gap-y-1 text-center">
+        <h1 className="text-xl leading-none font-bold">
+          Welcome to <pre className="inline">buckit</pre>
+        </h1>
+        <p className="text-sm">
+          Don't have an account?{" "}
+          <Link to="/register" className="font-medium underline">
+            Sign Up
+          </Link>
+        </p>
+      </section>
+      <form className="grid gap-y-3">
+        <fieldset className="grid gap-y-1.5">
+          <Label htmlFor={email}>Email</Label>
+          <Input
+            type="email"
+            name="email"
+            id={email}
+            placeholder="johndoe@gmail.com"
+          />
+        </fieldset>
+        <fieldset className="grid gap-y-1.5">
+          <Label htmlFor={password}>Password</Label>
+          <Input
+            type="password"
+            name="password"
+            id={password}
+            placeholder="********"
+          />
+        </fieldset>
+        <Button type="submit" className="mt-3">
+          Login
+        </Button>
+      </form>
+      <Separator />
+      <section className="flex justify-between">
+        <Button variant="outline">
+          <Icon icon="bxl:apple" />
+          Continue with Apple
+        </Button>
+        <Button variant="outline">
+          <Icon icon="bxl:google" />
+          Continue with Google
+        </Button>
+      </section>
+      <p className="text-muted-foreground text-center text-xs text-balance [&>a]:underline">
+        By clicking continue, you agree to our{" "}
+        <Link to=".">Terms of Service</Link> and{" "}
+        <Link to=".">Privacy Policy</Link>
+      </p>
+    </main>
+  );
 }
